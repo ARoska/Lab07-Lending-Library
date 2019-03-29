fac7cce6a3616c61161f4b71380921b3c92fd6f1
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-28 22:57:03 -0700

    Creates custom Generic Class with Add, Delete and Count.

--- LendingLibrary/LendingLibrary/Classes/Library.cs
+++ LendingLibrary/LendingLibrary/Classes/Library.cs
@@ -7,5 +7,64 @@ namespace LendingLibrary.Classes
 {
     class Library<T> : IEnumerable
     {
+        T[] books = new T[15];
+        int count = 0;
+
+        public void Add(T number)
+        {
+            if (count == books.Length)
+            {
+                Array.Resize(ref books, 5);
+            }
+            books[count++] = number;
+            count++;
+        }
+
+        public void Remove(T book)
+        {
+            T[] temp = null;
+            int index = Array.IndexOf(books, book);
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (i < index)
+                {
+                    temp[i] = books[i];
+                }
+                if (i == index)
+                {
+                    i++;
+                }
+                else if (i > index)
+                {
+                    temp[i - 1] = books[i - 1];
+                }
+            }
+            books = temp;
+
+            count--;
+            if (count == (books.Length - 5))
+            {
+                Array.Resize(ref books, 5);
+            }
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
